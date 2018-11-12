534a8b31528d6d8642d9392cac8a16b91905ae9d
Marcin Karwacki <kreatywny20@wp.pl>
2018-11-12 18:06:03 +0100

    small refactor + add categories + random choose + category display + media queries

--- game.js
+++ game.js
@@ -1,25 +1,49 @@
-let motto = "superman is superhero";
+let motto = "";
+let categories = [
+    ["gladiator", "star wars", "alien", "the godfather", "mamma mia", "pretty woman", "terminator", 'titanic'],
+    ["gta", "fifa", "quake", "far cry", "the witcher", 'heroes', 'fallout', 'bioshock'],
+    ["gdynia", "milan", "rome", "warsaw", "barcelona", 'paris', 'madrid', 'lisboa']
+];
+let chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+motto = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+console.log(motto);
 motto = motto.toLowerCase().split("");
+
 let mottoHidden = "";
 let lettersArray = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
 let noHitIt = 0;
+let maxHits = 7;
+
+let graphicWrapper = document.getElementById('graphic-id');
+let mottoWrapper = document.getElementById('motto-id');
+let lettersWrapper = document.getElementById('letters-id');
+let categoryName = document.getElementById('category-name');
+
+(function () {
+    if (chosenCategory === categories[0]) {
+        categoryName.innerHTML = "Chosen category is films";
+    } else if (chosenCategory === categories[1]) {
+        categoryName.innerHTML = "Chosen category is games";
+    } else if (chosenCategory === categories[2]) {
+        categoryName.innerHTML = "Chosen category is cities";
+    }
+})();
 
 for (let i = 0; i < motto.length; i++) {
     motto[i] === " " ? mottoHidden = `${mottoHidden} ` : mottoHidden = `${mottoHidden}-`;
 }
 
 function showMotto() {
-    document.getElementById("motto-id").innerHTML = mottoHidden;
+    mottoWrapper.innerHTML = mottoHidden;
 }
-
 
 function letters() {
     let alphabet = "";
     for (let i = 0; i < 26; i++) {
-        let element = "letter_" + i;
+        let element = `letter_${i}`;
         alphabet = alphabet + '<div class="letter-normal" id="' + element + '" onclick="checkIt(' + i + ')"><div class="greater-width">' + lettersArray[i] + '</div></div>';
     }
-    document.getElementById("letters-id").innerHTML = alphabet;
+    lettersWrapper.innerHTML = alphabet;
     showMotto();
 }
 
@@ -33,36 +57,36 @@ String.prototype.changeDash = function (index, letter) {
     }
 };
 
-
 function checkIt(num) {
     let hitIt = false;
     let element = "letter_" + num;
+    let buttonsWrapper = document.getElementById(element);
     for (let i = 0; i < motto.length; i++) {
         if (motto[i] === lettersArray[num]) {
             mottoHidden = mottoHidden.changeDash(i, lettersArray[num]);
             hitIt = true;
         }
     }
-    if (hitIt === true) {
-        document.getElementById(element).classList.add('letter-green');
+    if (hitIt) {
+        buttonsWrapper.classList.add('letter-green');
         showMotto();
     } else {
-        document.getElementById(element).classList.add('letter-red');
-        document.getElementById(element).setAttribute('onclick', '');
+        buttonsWrapper.classList.add('letter-red');
+        buttonsWrapper.setAttribute('onclick', '');
         noHitIt++;
-        document.getElementById('graphic-id').classList.remove(`graphic_${noHitIt - 1}`);
-        document.getElementById('graphic-id').classList.add(`graphic_${noHitIt}`);
+        graphicWrapper.classList.remove(`graphic_${noHitIt - 1}`);
+        graphicWrapper.classList.add(`graphic_${noHitIt}`);
     }
 
     if (motto.join("") === mottoHidden) {
-        document.getElementById('letters-id').classList.remove('letters');
-        document.getElementById('letters-id').classList.add('after-letters');
-        document.getElementById('letters-id').innerHTML = '<p class="smaller">Perfect! This is it: </p>' + motto.join("") + '<br><br><span class="reset" onclick="location.reload()"> Play again</span>';
+        lettersWrapper.classList.remove('letters');
+        lettersWrapper.classList.add('after-letters');
+        lettersWrapper.innerHTML = '<p class="smaller">Perfect! This is it: </p>' + motto.join("") + '<br><br><span class="reset" onclick="location.reload()"> Play again</span>';
     }
 
-    if (noHitIt >= 8) {
-        document.getElementById('letters-id').classList.remove('letters');
-        document.getElementById('letters-id').classList.add('after-letters');
-        document.getElementById('letters-id').innerHTML = '<p class="smaller">Nope! It should be: </p>' + motto.join("") + '<br><br><span class="reset" onclick="location.reload()"> Play again</span>';
+    if (noHitIt > maxHits) {
+        lettersWrapper.classList.remove('letters');
+        lettersWrapper.classList.add('after-letters');
+        lettersWrapper.innerHTML = '<p class="smaller">Nope! It should be: </p>' + motto.join("") + '<br><br><span class="reset" onclick="location.reload()"> Play again</span>';
     }
 }
